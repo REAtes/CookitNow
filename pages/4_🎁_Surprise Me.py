6e574dea5ed160151d5bea04174bc033ab12ff2e
mhelva <m.helva34@gmail.com>
2023-09-08 14:31:27 +0300

    Surprise me kısmına bir kaç küçük ekleme yapıldı. Bu sayfa için df oluşturuldu.

--- pages/4_🎁_Surprise Me.py
+++ pages/4_🎁_Surprise Me.py
@@ -68,9 +68,9 @@ cse_id = "c42eb241a8bb244c0"
 
 for a in random.sample(range(0, len(df_surprise)), 5): #range kısmı df'e göre değişecek
     st.subheader(name[a].title())
-    image_url1 = google_image_search(name[a], api_key, cse_id)
-    print(image_url1)
-    st.image(image_url1, caption=name[a], use_column_width="auto")
+    #image_url1 = google_image_search(name[a], api_key, cse_id)
+    #print(image_url1)
+    #st.image(image_url1, caption=name[a], use_column_width="auto")
     tab1, tab2, tab3 , tab4 = st.tabs(["Ingredients", "Cooking Steps", "Calori & Carbon Footprint", "Allergen"])
     with tab1:
         st.write(ingredients[a])
