9c526f2b547f35b6fe723f5a659ac4aa6ed62cc9
REAtes <r.emreates@gmail.com>
2023-09-13 20:30:32 +0200

    deploy içi deneme

--- pages/3_🍽️_All Recipes.py
+++ pages/3_🍽️_All Recipes.py
@@ -26,7 +26,7 @@ st.set_page_config(page_title="All Recipes | GastroMiuul", page_icon="🍽️")
 #     df = pd.read_csv(path)
 #     return df
 
-df = pd.read_csv(r".datasets/other_recipes_detailed.csv")
+df = pd.read_csv(r".datasets\other_recipes_detailed.csv")
 # df[df["name"].str.contains('TOTALLY TEMPESTUOUS')]
 # df[df["name"] == "VEGETARIAN"]
 df = df.loc[df["name"] != "SPICY SWEET ONION RINGS"]
